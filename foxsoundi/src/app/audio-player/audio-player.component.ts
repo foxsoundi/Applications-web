58af3d543f52fc5dc7b486044f052773f5b45e72
Loghan <loghan.r@gmail.com>
2019-05-21 16:28:42 +0200

    Auto stash before merge of "master" and "origin/master"
    Personalise audio player first step

--- foxsoundi/src/app/audio-player/audio-player.component.ts
+++ foxsoundi/src/app/audio-player/audio-player.component.ts
@@ -6,16 +6,57 @@ import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
   styleUrls: ['./audio-player.component.css']
 })
 export class AudioPlayerComponent implements OnInit {
+  audioSource = 'assets/audio/audio_dasha.mp3';
 
   constructor() { }
 
-  ngOnInit() {
-  }
-
-  audioSource = 'assets/audio/audio_dasha.mp3';
+  ngOnInit() { }
 
   @ViewChild('audioPlayer') audioplayer: ElementRef;
-  toggleAudio() {
+  @ViewChild('control') audioControl: ElementRef;
+  @ViewChild('progressBar') audioProgressBar: ElementRef;
+  /*toggleAudio() {
     this.audioplayer.nativeElement.play();
+  }*/
+
+  play() {
+    console.log(this.audioplayer);
+    const player = this.audioplayer.nativeElement;
+    const control = this.audioControl.nativeElement;
+
+    if (player.paused) {
+      player.play();
+      control.textContent = 'Pause';
+    } else {
+      player.pause();
+      control.textContent = 'Play';
+    }
+  }
+
+  resume() {
+    const player = this.audioplayer.nativeElement;
+
+    player.currentTime = 0;
+    player.pause();
+  }
+
+  volume(vol) {
+    const player = this.audioplayer.nativeElement;
+
+    player.volume = vol;
+  }
+
+
+  update() {
+    const player = this.audioplayer.nativeElement;
+    const duration = player.duration;    // Durée totale
+    const time     = player.currentTime; // Temps écoulé
+    const fraction = time / duration;
+    const percent  = Math.ceil(fraction * 100);
+
+    const progress = this.audioProgressBar.nativeElement;
+
+    progress.style.width = percent + '%';
+    progress.textContent = percent + '%';
   }
 }

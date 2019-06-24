a41378dee9c623992be96227bffc6b6ef121f108
Loghan <loghan.r@gmail.com>
2019-06-24 17:10:52 +0200

    Design suite (In progress)

--- foxsoundi/src/app/app.module.ts
+++ foxsoundi/src/app/app.module.ts
@@ -8,13 +8,17 @@ import { AudioPlayerComponent } from './audio-player/audio-player.component';
 import { LeftPanelComponent } from './left-panel/left-panel.component';
 import { MidPanelComponent } from './mid-panel/mid-panel.component';
 import { SpotifyService } from './spotify.service';
+import { GridComponent } from './mid-panel/grid/grid.component';
+import { ListComponent } from './mid-panel/list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AudioPlayerComponent,
     LeftPanelComponent,
-    MidPanelComponent
+    MidPanelComponent,
+    GridComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
